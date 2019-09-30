ca1bf998f3888f324e29d47ac5bb86dcb985b88e
DanFakePlasticTrees <34700952+DanFakePlasticTrees@users.noreply.github.com>
2019-09-30 12:25:24 -0300

    Add files via upload

---
+++ ABMAlumnosFull/ABMAlumnosFull/carrera.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+#ifndef CARRERA_H_INCLUDED
+#define CARRERA_H_INCLUDED
+typedef struct{
+
+    int id;
+    char descripcion[20];
+
+}eCarrera;
+
+#endif // CARRERA_H_INCLUDED
+
+
+void mostrarCarreras(eCarrera carreras[], int tam);
+void mostrarCarrera(eCarrera carrera);
+int cargarDescCarrera(int id, eCarrera carreras[], int tam, char desc[]);
---
+++ ABMAlumnosFull/ABMAlumnosFull/comida.c
@@ -0,0 +1,30 @@
+#include "comida.h"
+
+
+void mostrarComidas(eComida comidas[], int tam){
+    printf(" Id   Descripcion\n\n");
+    for(int i=0; i < tam; i++){
+        mostrarComida( comidas[i]);
+    }
+    printf("\n");
+}
+
+void mostrarComida(eComida comida){
+
+    printf("  %d      %10s    %4.2f\n", comida.id, comida.descripcion,precio);
+
+}
+
+int cargarDescComida(int id, eComida comidas[], int tam, char desc[]){
+    int todoOk = 0;
+
+    for(int i=0; i < tam; i++){
+        if( id == comidas[i].id){
+            strcpy(desc, comidas[i].descripcion);
+            todoOk = 1;
+            break;
+        }
+    }
+
+    return todoOk;
+}
